af787eac7605ee85fed440bb332f3947e9839d6f
Jobs <www@poboke.com>
2016-01-19 13:14:32 +0800

    Rename files.

--- ActivatePowerMode/HookClasses/IDESourceCodeEditor+HookPowerMode.m
+++ ActivatePowerMode/HookClasses/IDESourceCodeEditor+HookPowerMode.m
@@ -1,27 +1,27 @@
 //
-//  IDESourceCodeEditor+Hook.m
+//  IDESourceCodeEditor+HookPowerMode.m
 //  ActivatePowerMode
 //
 //  Created by Jobs on 15/12/1.
 //  Copyright © 2015年 Jobs. All rights reserved.
 //
 
-#import "IDESourceCodeEditor+Hook.h"
+#import "IDESourceCodeEditor+HookPowerMode.h"
 #import "ShakeAction.h"
 #import "SparkAction.h"
 #import "APMPlayer.h"
 
-@implementation IDESourceCodeEditor (Hook)
+@implementation IDESourceCodeEditor (HookPowerMode)
 
-+ (void)hook
++ (void)hookPowerMode
 {
     [self jr_swizzleMethod:@selector(textView:shouldChangeTextInRange:replacementString:)
-                withMethod:@selector(hook_textView:shouldChangeTextInRange:replacementString:)
+                withMethod:@selector(powerMode_textView:shouldChangeTextInRange:replacementString:)
                      error:nil];
 }
 
 
-- (BOOL)hook_textView:(NSTextView *)textView shouldChangeTextInRange:(NSRange)affectedCharRange replacementString:(NSString *)replacementString
+- (BOOL)powerMode_textView:(NSTextView *)textView shouldChangeTextInRange:(NSRange)affectedCharRange replacementString:(NSString *)replacementString
 {
     // Get the position under the cursor
     NSUInteger count = 0;
@@ -52,7 +52,7 @@
     
     [[APMPlayer defaultPlayer] playSound];
     
-    return [self hook_textView:textView shouldChangeTextInRange:affectedCharRange replacementString:replacementString];
+    return [self powerMode_textView:textView shouldChangeTextInRange:affectedCharRange replacementString:replacementString];
 }
 
 @end
